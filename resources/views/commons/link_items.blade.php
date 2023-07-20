f87737fbedc986461a8e1ca35dfb616fda863633
snake97123 <snake12379@yahoo.co.jp>
2023-07-20 08:47:41 +0000

    user pages

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,6 +1,6 @@
 @if(Auth::check())
-    <li><a class="link link-hover" href="#">Users</a></li>
-    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}&#39;s profile</a></li>
+    <li><a class="link link-hover" href="{{ route('users.index') }}">Users</a></li>
+    <li><a class="link link-hover" href="{{ route('users.show', Auth::user()->id) }}">{{ Auth::user()->name }}&#39;s profile</a></li>
     <li class="divider lg:hidden"></li>
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
