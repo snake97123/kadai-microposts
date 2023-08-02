bec2340a1305182b0f27d0a3619abba86ad7366b
snake97123 <snake12379@yahoo.co.jp>
2023-08-02 05:35:43 +0000

    post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,21 +3,22 @@
     <head>
         <meta charset="utf-8">
         <title>Microposts</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <!-- Scripts -->
         @vite('resources/css/app.css')
     </head>
-    
+
     <body>
+
+        {{-- ナビゲーションバー --}}
         @include('commons.navbar')
-        
+
         <div class="container mx-auto">
+            {{-- エラーメッセージ --}}
             @include('commons.error_messages')
-            
+
             @yield('content')
         </div>
-        
+
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,10 +28,8 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => ['auth']], function() {
-    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
-});
 
 Route::group(['middleware' => ['auth']], function() {
+    Route::resource('users', UsersController::class)->only(['index', 'show']);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
